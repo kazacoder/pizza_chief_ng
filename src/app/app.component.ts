40b05b6ce484c94d313eb5e4bfb676559bc6c10b
kazacoder <avtk.tlt@gmail.com>
2025-01-29 17:17:32 +0300

    Project refactored to Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,104 @@
 import { Component } from '@angular/core';
+import {ProductType} from "./types/product.type";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./css/animate.min.css', './css/hover-min.css', './css/magnific-popup.css', './app.component.scss', ],
 })
 export class AppComponent {
-  title = 'pizza_chief_ng';
+
+  public products: ProductType[] = [
+    {
+      image: 'pizza_image_1.png',
+      title: 'Мясная Делюкс',
+      description: `Пепперони, лук, бекон, томатная паста, колбаски, перец, грибы, соус чили, ананасы`
+    },
+    {
+      image: 'pizza_image_2.png',
+      title: 'Морская Премиум',
+      description: `Перец, сыр, креветки, кальмары, мидии, лосось`
+    },
+    {
+      image: 'pizza_image_3.png',
+      title: 'Бекон и Сосиски',
+      description: `Бекон, сыр, сосиски, ананас, томатная паста`
+    },
+    {
+      image: 'pizza_image_4.png',
+      title: 'Куриная Делюкс',
+      description: `Курица, ананас, сыр Пепперони, соус для пиццы, томатная паста`
+    },
+    {
+      image: 'pizza_image_5.png',
+      title: 'Барбекю Премиум',
+      description: `Свинина BBQ, соус Барбкею, сыр, курица, соус для пиццы, соус чили`
+    },
+    {
+      image: 'pizza_image_6.png',
+      title: 'Пепперони Дабл',
+      description: `Пепперони, сыр, колбаса 2 видов: обжаренная и вареная`
+    },
+    {
+      image: 'pizza_image_7.png',
+      title: 'Куриное трио',
+      description: `Жареная курица, Тушеная курица, Куриные наггетсы, перец, сыр, грибы, соус для пиццы`
+    },
+    {
+      image: 'pizza_image_8.png',
+      title: 'Сырная',
+      description: `Сыр Джюгас, Сыр с плесенью, Сыр Моцарелла, Сыр секретный`
+    },
+  ]
+
+  public formValues = {
+    productTitle: '',
+    address: '',
+    name: '',
+    phone: '',
+  }
+
+  public scrollTo(target: HTMLElement): void {
+    target.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  public addToCart(product: ProductType, target: HTMLElement): void {
+    this.scrollTo(target);
+    this.formValues.productTitle = product.title;
+  }
+
+  public createOrder() {
+    let inputs = Array.from(document.querySelectorAll('input'));
+    for (const input of inputs) {
+      if ((input as HTMLInputElement).value === '') {
+        alert(`Заполните поле ${(input as HTMLInputElement).placeholder}`);
+        return;
+      }
+    }
+
+    this.formValues = {
+      productTitle: '',
+      address: '',
+      name: '',
+      phone: '',
+    }
+
+  }
+
+  ngOnInit(): void {
+    const scripts = ['jquery-3.7.1.min.js', 'wow.min.js', 'jquery.magnific-popup.min.js', 'parallax.min.js', 'jquery.inputmask.min.js']
+
+
+    scripts.forEach(script => {
+      let node = document.createElement('script');
+      node.src = `assets/scripts/${script}`;
+      node.type = 'text/javascript';
+      node.async = true;
+      node.charset = 'utf-8';
+      document.getElementsByTagName('body')[0].appendChild(node);
+
+    })
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {NgOptimizedImage} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, HeaderComponent, FooterComponent]
 })
 export class AppModule { }
