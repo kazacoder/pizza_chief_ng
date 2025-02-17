a19cb2b9f37c27eb081c94fa893f869b56dd7402
user <user@test.net>
2025-02-17 09:21:53 +0300

    Module 12 lesson 15 Modules - completed

--- src/app/shared/directives/cool-input.directive.ts
+++ src/app/shared/directives/cool-input.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostBinding, HostListener, Injector, Input, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[coolInput]'
