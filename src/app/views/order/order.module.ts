bdfaf2a7e407f9a23fafb931a7a29b686f93040f
kazacoder <avtk.tlt@gmail.com>
2025-02-17 11:26:58 +0300

    Components libraries learning. Primefaces.

--- src/app/views/order/order.module.ts
+++ src/app/views/order/order.module.ts
@@ -6,6 +6,7 @@ import {OrderComponent} from "./order.component";
 import {SharedModule} from "../../shared/shared.module";
 import {RouterModule} from "@angular/router";
 import {FormsModule} from "@angular/forms";
+import {InputTextModule} from "primeng/inputtext";
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import {FormsModule} from "@angular/forms";
     SharedModule,
     RouterModule,
     FormsModule,
-    OrderRoutingModule
+    OrderRoutingModule,
+    InputTextModule
   ],
   exports: [
     OrderRoutingModule,
